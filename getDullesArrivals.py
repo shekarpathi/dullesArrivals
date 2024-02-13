1b25976bdc4f02b9eb96dff8d5eb3bf0ef784e7c
shekarpathi <tambaram@gmail.com>
2024-02-13 12:55:37 -0500

    Update getDullesArrivals.py

--- getDullesArrivals.py
+++ getDullesArrivals.py
@@ -35,13 +35,91 @@ arrivalsFile.write("""<link rel="stylesheet" type="text/css" href="https://cdn.d
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#example').DataTable();
+
+
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#example thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#example thead');
+ 
+    var table = $('#example').DataTable({
+    "autoWidth": false,
+        "columnDefs": [
+        {"className": "dt-left", "targets": "_all", "width": "5%"},
+      {
+        targets: 1,
+         width: 1
+      }
+      ],
+      
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+ 
+            // For each column
+            api
+                .columns()
+                .eq(0)
+                .each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+ 
+                    // On every keypress in this input
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('change', function (e) {
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+ 
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+                        })
+                        .on('keyup', function (e) {
+                            e.stopPropagation();
+ 
+                            $(this).trigger('change');
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
     });
+});
+
+
 </script>
+
+<style type="text/css" class="init">
+thead input {
+        width: 100%;
+    }
+</style>
+
 </head>\n""")
 
-arrivalsFile.write('<table data-order=\'[[ 5, "asc" ]]\' data-page-length=\'15\' id="example" class="display" style="width:100%">\n')
+arrivalsFile.write('<table data-order=\'[[ 5, "asc" ]]\' data-page-length=\'20\' id="example" class="cell-border" style="width:100%">\n')
 arrivalsFile.write("""<thead>
             <tr>
                 <th>Airline</th>
