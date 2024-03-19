95d2513bb847c4fab4358f977f8a664f7a1cff91
shekarpathi <tambaram@gmail.com>
2024-03-19 18:42:04 -0400

    Update createAirportCode.py

--- createAirportCode.py
+++ createAirportCode.py
@@ -2,10 +2,6 @@ import csv
 import re
 
 # "id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","iso_country","iso_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords"
-# 6523,"00A","heliport","Total RF Heliport",40.070985,-74.933689,11,"NA","US","US-PA","Bensalem","no","K00A",,"00A","https://www.penndot.pa.gov/TravelInPA/airports-pa/Pages/Total-RF-Heliport.aspx",,
-# 323361,"00AA","small_airport","Aero B Ranch Airport",38.704022,-101.473911,3435,"NA","US","US-KS","Leoti","no","00AA",,"00AA",,,
-# 5226,"OMAA","large_airport","Abu Dhabi International Airport",24.443764,54.651718,88,"AS","AE","AE-AZ","Abu Dhabi","yes","OMAA","AUH",,"http://www.abudhabiairport.ae/","https://en.wikipedia.org/wiki/Abu_Dhabi_International_Airport",
-
 
 def readAirportCodesCsv() -> dict:
     airportdict: dict = {}
