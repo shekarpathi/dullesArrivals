91d826065fb2dd22d64e77eda8dce3b072dfcc14
shekarpathi <tambaram@gmail.com>
2024-07-24 17:18:41 -0400

    Changes

--- console_1.sql
+++ console_1.sql
@@ -1,5 +1,3 @@
-select * from ARRIVALS where DATE(publishedTime) = DATE('now') order by actualtime desc;
-
 SELECT ARRIVALS.actualtime, ARRIVALS.publishedtime,
     case when ARRIVALS.actualtime is null
         THEN
@@ -21,11 +19,12 @@ SELECT ARRIVALS.actualtime, ARRIVALS.publishedtime,
     else
         ARRIVALS.status
     end as status2,
-
-    'https://www.flydulles.com/flight/arrival/'||DATE('now')||'/'||ARRIVALS.IATA||'/'||ARRIVALS.flightnumber AS URL,
+    AIRLINES.ICAO,
+    'https://www.flydulles.com/flight/arrival/'||DATE('now')||'/'||ARRIVALS.IATA||'/'||ARRIVALS.flightnumber AS FlyDullesURL,
+    'https://www.flightradar24.com/data/aircraft/'||ARRIVALS.tail_number AS equipment_history,
     case when substr(timediff(actualtime, datetime('now', 'localtime')), 1, 1) == '-'
         Then substr(timediff(actualtime, datetime('now', 'localtime')), 12, 6) || ' Ago'
         Else 'In' || substr(timediff(actualtime, datetime('now', 'localtime')), 12, 6)
         END AS TimeDifference, status,
     *
-       from ARRIVALS where DATE(publishedTime) = DATE('now') order by actualtime asc;
+       from ARRIVALS, AIRLINES where DATE(publishedTime) = DATE('now')  and ARRIVALS.IATA = AIRLINES.IATA order by actualtime asc;
