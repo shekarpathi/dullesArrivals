99a9223a47476bb9baad06a4b4fc366002c63723
shekarpathi <tambaram@gmail.com>
2025-04-25 16:09:29 -0400

    Added timeout to requests

--- globals.py
+++ globals.py
@@ -6,12 +6,14 @@ def init():
     print("Initializing global variables...")
     token_url = "https://www.united.com/api/auth/anonymous-token"
     headers_token = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:137.0) Gecko/20100101 Firefox/137.0",
+        "User-Agent": "Mozilla/5.0 Gecko/20100101 Firefox/133.0",
         "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
         "Sec-Fetch-Site": "same-origin",
         "Priority": "u=0"
     }
-    response = requests.get(token_url, headers=headers_token)
+    response = requests.get(token_url, headers=headers_token, timeout=(5,10)) # 5 seconds connect timeout, 10 seconds read timeout
     response.raise_for_status()
     bearer_token = response.json().get("data", {}).get("token", {}).get("hash")
 
