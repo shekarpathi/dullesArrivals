38fe0561383af6d26c7aa4fc9ce2983350174be4
Shekar <tambaram@gmail.com>
2025-06-29 11:05:57 -0400

    Initial commit

---
+++ cp_new.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/ec2-user/dullesArrivals
+git fetch
+git pull
+/usr/bin/python3 shekar.py
+sudo rm -f /var/www/html/fis.html /var/www/html/iab.html /var/www/html/index.html /var/www/html/arrivals.html /var/www/html/arrivals.json /var/www/html/departures.html /var/www/html/departures.json
+sudo cp iab.html /var/www/html
+sudo cp index.html /var/www/html
+sudo cp arrivals.html /var/www/html
+sudo cp departures.html /var/www/html
+sudo cp arrivals.json /var/www/html
+sudo cp departures.json /var/www/html
+sudo cp *.png /var/www/html
+sudo cp favicon.ico /var/www/html
+sudo chown ec2-user /var/www/html/*
+sudo chgrp ec2-user /var/www/html/*
+date >> /tmp/lastRunTimestamp
+date >> /var/www/html/lastRunTimestamp
